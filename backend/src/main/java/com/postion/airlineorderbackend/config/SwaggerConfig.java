7c4d5a0faf6bf9e93a7c9583aaada85f92bacf94
daixth <daixth@cn.ibm.com>
2025-11-04 10:15:48 +0800

    Update SwaggerConfig.java

--- backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
+++ backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
@@ -36,7 +36,7 @@ public class SwaggerConfig {
                                 .url("https://github.com/wang-rui-rae"))
                        )
                 .servers(java.util.Arrays.asList(
-                        new Server().url("http://13.51.150.144:8080").description("开发环境"),
+                        new Server().url("http://13.60.21.225:8080").description("开发环境"),
                         new Server().url("https://api.airline.com").description("生产环境")
                 ))
 
